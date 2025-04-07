8d8900bdf28606a7760b66b5e34f220989622f06
ccc0303ccc <109770257+ccc0303ccc@users.noreply.github.com>
2025-04-07 15:04:48 +0800

    Create ssts.js

---
+++ weisyr/plug/ssts.js
@@ -0,0 +1,22 @@
+push_tips = "完美支持了大体积的gif和svg";
+//eval(request("https://api.vvhan.com/api/denglong"));
+/*fba.writeFile('hiker://files/rules/dzHouse/html/plug/文件管理.js', request('http://123.56.105.145/weisyr/plug/文件管理.js'));
+*/
+/*
+fba.parseLazyRule($$$().lazyRule(() => {
+	deleteFile('file:///storage/emulated/0/Android/data/com.example.hikerview/files/Documents/cache/顺搜_默认图.jpg');
+},默认图))
+*/
+update(03281)
+/*
+audio = new Audio("https://raw.githubusercontent.com/ccc0303ccc/Sky/main/weisyr/video/天气预报bgm.mp3");
+$("#weather_info").click(() => {
+audio.volume = 0.6;
+audio.play();
+});
+
+push_img = "https://p4.itc.cn/q_70/images03/20210730/ba67cd0a49494b76bb7e830845921962.gif";
+push_url = "https://tiyu.baidu.com/al/major/home?page=home&match=2024%E5%B9%B4%E5%B7%B4%E9%BB%8E%E5%A5%A5%E8%BF%90%E4%BC%9A&tab=%E5%A5%96%E7%89%8C%E6%A6%9C";
+push_rule = `js:var d=[];d.push({desc:'auto&&float',col_type:'x5_webview_single',url:'${push_url}'});setResult(d)`;
+push("08-12");
+*/
