cf4c13fc1864f1ee9f92464416f15bdd131db1bf
ccc0303ccc <109770257+ccc0303ccc@users.noreply.github.com>
2025-06-01 01:12:34 +0800

    Update 𝐌𝐢𝐬𝐬𝐀𝐕.js

--- 𝐌𝐢𝐬𝐬𝐀𝐕.js
+++ 𝐌𝐢𝐬𝐬𝐀𝐕.js
@@ -966,7 +966,7 @@ const Apollo = {
             var title = pdfh(item, '.lozad&&alt');
             Apollo.d.push({
                 title: title,
-                url: $(pdfh(item, 'a&&href') + '#noHistory#').rule(() => {
+                url: $(pdfh(item, 'a&&href') + '#noHistory##gameTheme#').rule(() => {
                     const Apollo = $.require('hiker://page/Apollo')
                     Apollo.videoParse(MY_URL)
                     setResult(Apollo.d)
@@ -997,7 +997,7 @@ const Apollo = {
         list.forEach(item => {
             Apollo.d.push({
                 title: pdfh(item, 'img&&alt'),
-                url: $(pdfh(item, 'a&&href') + '#noHistory#').rule(() => {
+                url: $(pdfh(item, 'a&&href') + '#noHistory##gameTheme#').rule(() => {
                     const Apollo = $.require('hiker://page/Apollo')
                     const html = fetch(MY_URL, {
                         headers: {
@@ -1155,7 +1155,7 @@ const Apollo = {
             var dc = storage0.getItem(mark, '0') == '0' ? sdesc : desc;
             var cs = storage0.getItem(mark, '0') == '0' ? colors.hide : colors.show;
             arr.push({
-                title: '☠' + '<b><font color="#FFBF00">劇情簡介	</font></b>' + "<middle><a style='text-decoration: none;' href='" + lazy + "'>" + sc + '</a></big><br><font color="' + cs + '">' + `${dc}` + '</small>',
+                title: '☠' + '<b><font color="#FFBF00">劇情簡介	</font></b>' + "<middle><a style='text-decoration: none;' href='" + lazy + "'>" + sc + '</a></small><br><font color="' + cs + '">' + `${dc}` + '</small>',
                 col_type: 'rich_text',
                 extra: {
                     id: 'desc',
